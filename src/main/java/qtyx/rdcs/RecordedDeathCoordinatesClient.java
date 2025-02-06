f37aa865f149e07b2e67665570b45915a0f91cb1
QTYXOVO <ktr2427@qq.com>
2025-02-06 09:43:41 +0800

    Initial commit

---
+++ src/main/java/qtyx/rdcs/RecordedDeathCoordinatesClient.java
@@ -0,0 +1,10 @@
+package qtyx.rdcs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RecordedDeathCoordinatesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
